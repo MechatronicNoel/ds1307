04cb8ef762b044bc95b4067b144369641754cf20
Noel <imnooee1@gmail.com>
2021-09-18 10:42:42 -0500

    write/read function created and tested

--- inc/ds1307.h
+++ inc/ds1307.h
@@ -1,17 +1,55 @@
-/*
- * ds1307.h
+/**
+ * @copyright (c) 2021 Noel Dom https://www.youtube.com/channel/UCINCDcQylATh2wS5BAYIBPg.
  *
- *  Created on: Sep 17, 2021
- *      Author: nooee
+ * @brief	Device Driver for DS1307 Real-time clock (RTC)
+ * @file	ds1307.h
+ * @version 0.1.0
+ * @date	2021
+ * @author 	Noel Dominguez.
  */
-
 #ifndef DS1307_H_
 #define DS1307_H_
 
 #include "i2c.h"
 #include "DBG.h"
-void start_i2c_scan(I2C_HandleTypeDef *I2Chnd, uint32_t delay_);
 
+typedef struct {
+
+	uint8_t seconds;
+	uint8_t minutes;
+	uint8_t hours;
+	uint8_t day;
+	uint8_t date;
+	uint8_t month;
+	uint8_t year;
+
+}ds1307_dev_t;
+
+
+typedef enum{
+	DS1307_OK,
+	DS1307_ERROR
+}ds1307_err_t;
+
+
+/**
+ * @brief I2C Scanner utility .
+ *
+ * @param[in]	I2Chnd: i2c handler
+ * @param[in]	delay_: delay between each device scan
+ *
+ */
+void start_i2c_scan(I2C_HandleTypeDef *I2Chnd, uint32_t delay_);
+void ds1307_init(void);
+ds1307_err_t ds1307_set_hour(uint8_t hour);
+uint8_t ds1307_get_hour(void);
+ds1307_err_t ds1307_set_hour(uint8_t hour);
+void ds1307_set_clock_halt(uint8_t halt);
+uint8_t ds1307_get_clock_halt(void);
+void ds1307_set_second(uint8_t second);
+uint8_t ds1307_get_second(void);
+void ds1307_set_minutes(uint8_t minutes);
+uint8_t ds1307_get_minutes(void);
 
 
 #endif /* DS1307_H_ */
--- src/ds1307.c
+++ src/ds1307.c
@@ -1,13 +1,17 @@
-/*
- * ds1307.c
+/**
+ * @copyright (c) 2021 Noel Dom https://www.youtube.com/channel/UCINCDcQylATh2wS5BAYIBPg.
  *
- *  Created on: Sep 17, 2021
- *      Author: nooee
+ * @brief	Device Driver for DS1307 Real-time clock (RTC)
+ * @file	ds1307.c
+ * @version 0.1.0
+ * @date	2021
+ * @author 	Noel Dominguez.
  */
 
 
 #include "ds1307.h"
 
+#define DS1307_ADDRES		0x68
 #define DS1307_SECONDS 		0x00
 #define DS1307_MINUTES		0x01
 #define DS1307_HOURS		0x02
@@ -23,7 +27,7 @@
 #define DS1307_TIMEOUT		1000
 
 
-#define DS1307_HANDLER		h2ic1
+#define DS1307_HANDLER		hi2c1
 
 typedef enum{
 	DS1307_1HZ,
@@ -31,6 +35,77 @@ typedef enum{
 	DS1307_8192HZ,
 	DS1307_32768HZ
 }ds1307_rate_t;
+
+
+
+static ds1307_err_t ds1307_write_byte(uint8_t ds1307_reg_addres, uint8_t data);
+static uint8_t ds1307_read_byte(uint8_t ds1307_reg_addres);
+static uint8_t ds1307_bcd(uint8_t data);
+
+
+static ds1307_err_t ds1307_write_byte(uint8_t ds1307_reg_addres, uint8_t data){
+
+	uint8_t buff[2] = {ds1307_reg_addres,data};
+	ds1307_err_t ret_val;
+	ret_val = HAL_I2C_Master_Transmit(&DS1307_HANDLER, DS1307_ADDRES << 1, buff, 2, DS1307_TIMEOUT);
+
+	return ret_val;
+}
+
+static uint8_t ds1307_read_byte(uint8_t ds1307_reg_addres){
+
+	uint8_t data;
+	HAL_I2C_Master_Transmit(&DS1307_HANDLER, DS1307_ADDRES << 1, &ds1307_reg_addres, 1, DS1307_TIMEOUT);
+	HAL_I2C_Master_Receive(&DS1307_HANDLER, DS1307_ADDRES << 1, &data, 1, DS1307_TIMEOUT);
+	return data;
+}
+
+static uint8_t ds1307_bcd(uint8_t data){
+	return (((data & 0xf0) >> 4) * 10) + (data & 0x0f);
+}
+
+
+void ds1307_init(void){
+	ds1307_set_clock_halt(0);
+}
+
+
+void ds1307_set_clock_halt(uint8_t halt){
+	uint8_t ch = (halt ? 1 << 7 : 0);
+	ds1307_write_byte(DS1307_SECONDS, ch | (ds1307_read_byte(DS1307_SECONDS) & 0x7F));
+}
+
+uint8_t ds1307_get_clock_halt(void){
+	return (ds1307_read_byte(DS1307_SECONDS) & 0x80) >> 7;
+}
+
+
+ds1307_err_t ds1307_set_hour(uint8_t hour){
+	return ds1307_write_byte(DS1307_HOURS,ds1307_bcd(hour & 0x3F));
+}
+
+uint8_t ds1307_get_hour(void){
+	return ds1307_bcd (ds1307_read_byte(DS1307_HOURS) & 0x3F);
+}
+
+
+void ds1307_set_second(uint8_t second){
+	uint8_t val = ds1307_get_clock_halt();
+	ds1307_write_byte(DS1307_SECONDS, ds1307_bcd(second | val));
+
+}
+
+uint8_t ds1307_get_second(void){
+	return ds1307_bcd(ds1307_read_byte(DS1307_SECONDS) & 0x7F);
+}
+
+void ds1307_set_minutes(uint8_t minutes){
+	ds1307_write_byte(DS1307_MINUTES, ds1307_bcd(minutes));
+}
+
+uint8_t ds1307_get_minutes(void){
+	return ds1307_read_byte(ds1307_bcd(DS1307_MINUTES));
+}
 
 void start_i2c_scan(I2C_HandleTypeDef *I2Chnd, uint32_t delay_){
 
